827e2259ae647f403cca91c835a5187cb02e4573
achriziq <85885410+achriziq@users.noreply.github.com>
2021-06-14 23:20:05 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ bot.onText(/\/start/, (msg) => {
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        `hello ${msg.chat.first_name}, welcome Achmad Riziq Fathurohman \n
+        click /predict untuk prediksi`
     );   
 });
 
@@ -27,6 +27,47 @@ bot.onText(/\/menu/, (msg) => {
         `this is your main menu`
     );   
 });
+
+
+state = 0;
+bot.onText(/\/predict/, (msg) => {
+    bot.sendMessage(
+        msg.chat.id,
+        'masukan nilai i|v contohnya 7|7' 
+    );
+    state = 1;
+});
+
+bot.on('message',(msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]),
+                parseFloat(s[1])
+                
+            ]
+            
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                'nilai p yang di prediksi adalah ${jres[0]} volt'
+                );
+            bot.sendMessage(
+                msg.chat.id,
+                'nilai p yang di prediksi adalah ${jres[1]} watt'
+                
+            );
+            
+        })
+        
+    }else{
+        state = 0
+    }
+    
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    

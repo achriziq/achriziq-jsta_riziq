2f8511f6b3ae8c5067c66fa55b9e5bab644a6092
achriziq <85885410+achriziq@users.noreply.github.com>
2021-06-18 17:14:27 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -39,7 +39,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/achriziq/achriziq-jsta_riziq/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/achriziq/achriziq-jsta_riziq/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
